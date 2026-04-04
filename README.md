6bb9fe0b64c805c9f91c8e2a7bb4a26cc38d39d2
annagna <annagna08@gmail.com>
2026-04-03 23:26:40 -0300

    adicionei legendas no readme e acrescentei no js

--- README.md
+++
@@ -1,4 +0,0 @@
-Projeto de site para Vinícola Agrello |
-Programação para cadastro e organização dos vinhos da vinícola Agrello |
-Nome dos integrantes: Anna Giazzi, Emily Rebeca e Giulia Stella |
-Link do GitHubs Pages: 
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,8 @@
-var Nome = prompt('Digite seu nome: ');
-var Sobrenome = prompt('Digite seu sobrenome: ');
+
+  var botao = document.getElementById("meuBotao");
+  botao.addEventListener("click", function() {
+    var Nome = prompt('Digite seu nome: ');
+    var Sobrenome = prompt('Digite seu sobrenome: ');
+    var Email = prompt('Digite seu email: ');
+    alert("Cadastro realizado com sucesso!");
+  });
